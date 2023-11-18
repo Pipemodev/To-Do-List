2ad52516f261332f2a3cce511384308fcaaca8cb
Pipemodev <75000016+Pipemodev@users.noreply.github.com>
2023-11-18 18:42:26 -0500

    HTML selectors created

---
+++ app.js
@@ -0,0 +1,4 @@
+// Seleccionar los elementos HMTL.
+const input = document.getElementById('ingresar-tarea');
+const boton = document.querySelector('button');
+const listaDeTareas = document.getElementById('lista-de-tareas');
